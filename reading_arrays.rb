5c9995d63588faf62b4522add493fc7cbb769eb5
dami21 <dami.adenekan@schaumburgchristian.com>
2018-08-09 14:10:48 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -12,12 +12,12 @@ end
 first_student_by_index
 
 def fourth_student_by_index
-  puts STUDENT_NAMES[3]
+  return STUDENT_NAMES[3]
 end
 fourth_student_by_index
 
 def last_student_by_index
-  puts STUDENT_NAMES.last
+  return STUDENT_NAMES.last
 end
 last_student_by_index
 
